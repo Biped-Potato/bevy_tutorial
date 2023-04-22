d9fb5c3977a0fccb260f7ae93f0eaf817529784c
Platypus88 <79812179+Platypus88@users.noreply.github.com>
2023-04-22 13:23:14 -0500

    Step 3 and 4 :( misclick

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,21 @@ pub fn create_player_anim_hashmap()->HashMap<String,animation::Animation>
 
     return hash_map;
 }
+pub fn create_gun_anim_hashmap()->HashMap<String,animation::Animation>
+{
+    let mut hash_map = HashMap::new();
+
+    hash_map.insert("Shoot".to_string(),animation::Animation{start:1,end:5,looping:true,cooldown:0.1});
+
+    hash_map.insert("Idle".to_string(),animation::Animation{start:1,end:1,looping:true,cooldown:0.1});
+
+    return hash_map;
+}
+
 pub fn setup(mut commands: Commands, asset_server : Res<AssetServer>, mut texture_atlases : ResMut<Assets<TextureAtlas>>)
 {
-    let texture_handle = asset_server.load("player.png");
-    let texture_atlas = TextureAtlas::from_grid(
+    let mut texture_handle = asset_server.load("player.png");
+    let mut texture_atlas = TextureAtlas::from_grid(
         texture_handle,
         Vec2::new(8.0+1.0,9.0+1.0),
         3,
@@ -33,7 +44,7 @@ pub fn setup(mut commands: Commands, asset_server : Res<AssetServer>, mut textur
         Some(Vec2::new(1.,1.)),
         None,
     );
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    let mut texture_atlas_handle = texture_atlases.add(texture_atlas);
     commands.spawn(Camera2dBundle::default());
     commands.spawn(SpriteSheetBundle{
         texture_atlas:texture_atlas_handle,
@@ -47,4 +58,30 @@ pub fn setup(mut commands: Commands, asset_server : Res<AssetServer>, mut textur
         animation_bank: create_player_anim_hashmap(),
     })
     .insert(player::PlayerMovement{speed:100.});
+
+    texture_handle = asset_server.load("gun.png");
+    texture_atlas = TextureAtlas::from_grid(
+        texture_handle,
+        Vec2::new(8.0+1.0,8.0+1.0),
+        5,
+        1,
+        Some(Vec2::new(1.,1.)),
+        None,
+    );
+    texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    commands.spawn(SpriteSheetBundle{
+        texture_atlas:texture_atlas_handle,
+        transform:Transform::from_scale(Vec3::splat(5.0)),
+        ..default()})
+    .insert(animation::Animator{
+        timer:0.,
+        cooldown:0.05,
+        last_animation : "Shoot".to_string(),
+        current_animation : "Shoot".to_string(),
+        animation_bank: create_gun_anim_hashmap(),
+    });
+
+
+
 }
--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::animation::Animator;
+
 #[derive(Component)]
 pub struct PlayerMovement
 {
@@ -7,27 +9,37 @@ pub struct PlayerMovement
 }
 
 pub fn move_player(
-    time: Res<Time>,
+    time : Res<Time>,
     keys: Res<Input<KeyCode>>,
-    mut query: Query<(&PlayerMovement,&mut Transform)>
-)
+    mut query: Query<(
+    &PlayerMovement,
+    &mut Transform,
+    &mut Animator,
+)>)
 {
-    for(player_movement, mut transform) in query.iter_mut()
+    for(player_movement, mut transform,mut animator) in query.iter_mut()
     {
+        animator.current_animation = "Idle".to_string();
         if keys.pressed(KeyCode::W)
         {
+            animator.current_animation = "Walk".to_string();
             transform.translation.y+=player_movement.speed*time.delta_seconds();
         }
         if keys.pressed(KeyCode::A)
         {
+            transform.rotation = Quat::from_rotation_y(std::f32::consts::PI);
+            animator.current_animation = "Walk".to_string();
             transform.translation.x-=player_movement.speed*time.delta_seconds();
         }
         if keys.pressed(KeyCode::S)
         {
+            animator.current_animation = "Walk".to_string();
             transform.translation.y-=player_movement.speed*time.delta_seconds();
         }
         if keys.pressed(KeyCode::D)
         {
+            transform.rotation = Quat::default();
+            animator.current_animation = "Walk".to_string();
             transform.translation.x+=player_movement.speed*time.delta_seconds();
         }
 
